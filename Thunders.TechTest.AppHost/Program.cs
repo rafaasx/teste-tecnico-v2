4543f686f4417ea0cfdeb9da8cdc61b58f0cf6dc
Rafael Xavier <rafael.x@lyncas.net>
2025-03-24 08:41:10 -0300

    Ajustado deploy

--- Thunders.TechTest.AppHost/Program.cs
+++ Thunders.TechTest.AppHost/Program.cs
@@ -4,15 +4,14 @@ var cache = builder.AddRedis("cache");
 
 var rabbitMqPassword = builder.AddParameter("RabbitMqPassword", true);
 
-var rabbitMq = builder.AddRabbitMQ("RabbitMq", password: rabbitMqPassword)
+var rabbitMq = builder.AddRabbitMQ("rabbitmq", password: rabbitMqPassword)
     .WithExternalHttpEndpoints()
-    .WithDataVolume("rabbitmq-data-volume")
-    .WithVolume("/etc/rabbitmq")
+    .WithDataVolume("rabbitmqdatavolume")
     .WithManagementPlugin();
 
 var sqlServerPassword = builder.AddParameter("SqlServerInstancePassword", true);
 var sqlServerPort = int.Parse(builder.AddParameter("SqlServerInstancePort", true).Resource.Value);
-var sqlServer = builder.AddSqlServer("SqlServerInstance", password: sqlServerPassword, port: sqlServerPort)
+var sqlServer = builder.AddSqlServer("sqlserverinstance", password: sqlServerPassword, port: sqlServerPort)
     .WithDataVolume("sqlserverinstance-volume");
 
 var database = sqlServer.AddDatabase("ThundersTechTestDb", "ThundersTechTest");
